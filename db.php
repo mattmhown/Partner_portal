f8fb6ace4be2bb43aa128a4835972e8e95ba437b
mattmhown <accounts@hown.com.au>
2025-01-31 18:39:26 +1000

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,14 @@
+<?php
+$servername = "localhost:3306";  // Change if your host is different
+$username = "admin"; 
+$password = "Bitemefear8"; 
+$dbname = "howncoma_PartnerPortal"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
 
